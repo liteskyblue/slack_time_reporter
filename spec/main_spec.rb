dfdc06d3c4581d24fb772e4f36223d1fa51e94ae
Gou Furuya <innocent.zero@gmail.com>
2016-07-11 20:49:11 +0900

    Add print make remind spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -30,6 +30,29 @@ describe TimeReporter do
         expect(TimeReporter.new.end_time?).to be_truthy
       end
     end
+  end
+
+  describe 'remind print make day or time' do
+    let(:sunday) { Time.local(2016, 6, 12) }
+    let(:print_make_day) { Time.local(2016, 6, 4, 12, 20) }
+
+    it 'not remind day' do
+      Timecop.freeze(sunday)
+      expect(TimeReporter.new.print_make_day?).to be_falsy
+    end
+
+    it 'remind day' do
+      Timecop.freeze(print_make_day)
+      expect(TimeReporter.new.print_make_day?).to be_truthy
+    end
+
+    it 'print make time' do
+      Timecop.freeze(print_make_day)
+      (0..9).each do |n|
+        Timecop.travel(print_make_day + n.minutes)
+        expect(TimeReporter.new.remind_time?).to be_truthy
+      end
+    end
 
     after { Timecop.return }
   end
